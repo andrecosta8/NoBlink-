0fc8b34e4cef6dc2155f75fc165640614fecc8f3
André Costa <andrecosta8@gmail.com>
2022-02-28 23:22:01 +0000

    shorter and clean code

--- js/index.js
+++ js/index.js
@@ -1,75 +1,51 @@
+let circles = [];
+let baseColor = "orange";
+let lightColor = "blue";
 let randomChoice;
-let baseColor;
-let lightColor;
-let score;
-//const reactionTime;
-//let circles = [circle(250, 250, 100), circle(550, 500, 100), circle(250, 500, 100), circle(550, 250, 100,)];
-// let leftUp;
-// let leftDown;
-// let rightUp;
-// let rightDown;
-//let arrayOfEllipse;
-/*
+let one;
+let two;
+let three;
+let four;
+
 class Circles {
-    constructor(x, y, width, height) {
+    constructor(x, y, width, heigth) {
         this.x = x;
         this.y = y;
         this.width = width;
-        this.height = height;
+        this.heigth = heigth;
     }
-    draw() {
+    display() {
         fill(baseColor);
-        circle(this.x, this.y, this.width, this.height);
-        
+        ellipse(this.x, this.y, this.width, this.heigth);
+    }
+    change() {
+        fill(lightColor);
+        ellipse(this.x, this.y, this.width, this.heigth);
     }
 }
-leftUp = new Circles(250, 250, 100, 100);
-leftDown = new Circles(250, 500, 100, 100);
-rightUp = new Circles(550, 250, 100, 100);
-rightDown = new Circles(550, 500, 100, 100);
-*/
-//console.log(Circles);
 
-
-function setup() {  
-    baseColor = "white";
-    lightColor = "blue";
+function setup() {
     createCanvas(900, 900);
     //canvas.parent("game-board");
+    one = circles[0] = new Circles(250, 250, 100, 100);
+    two = circles[1] = new Circles(250, 500, 100, 100);
+    three = circles[2] = new Circles(550, 250, 100, 100);
+    four = circles[3] = new Circles(550, 500, 100, 100);
 
 }
-
 function draw() {
     background(50);
-      
-    /*
-    leftUp.draw();
-    leftDown.draw();
-    rightUp.draw();
-    rightDown.draw();
-    */
-    
-    fill(baseColor);
-    if (randomChoice === 0) {
-        fill(lightColor);
+    for (let i = 0; i < circles.length; i++) {
+        if (randomChoice === i) {
+            circles[i].change();
+        } else {
+            circles[i].display();
+        }
     }
-    circle(250, 250, 100)
-    fill(baseColor);
-    if (randomChoice === 1) {
-        fill(lightColor);
-    }
-    circle(550, 500, 100)
-    fill(baseColor);
-    if (randomChoice === 2) {
-        fill(lightColor);
-    }
-    circle(250, 500, 100)
-    fill(baseColor);
-    if (randomChoice === 3) {
-        fill(lightColor);
-    }
-    circle(550, 250, 100)
-    
+}
+
+function randomCircle() {
+    return randomChoice = Math.floor(Math.random() * circles.length);
 }
 
 function startGame() {
@@ -77,35 +53,11 @@ function startGame() {
 }
 
 function mousePressed() {
-   
-  }
-
-function randomEllipse() {
-    return randomChoice = Math.floor(Math.random() * 4);
-}
-
-setInterval(randomEllipse, 1000);
-
-/*
-function colorChange(randomChoice) {
-    if(randomChoice === 0){
-     
-    }
-    if(randomChoice === 1){
-        
-        fill(lightColor);
-    }
-    if(randomChoice === 2){
-        
-        fill(lightColor);
-    }
-    if(randomChoice === 3){
-        
-        fill(lightColor);
-    }
 
 }
-*/
+
+
+//setInterval(randomCircle, 1000);
 
 /*
 window.onload = () => {
@@ -117,5 +69,4 @@ window.onload = () => {
     };
 }
 */
-
 
